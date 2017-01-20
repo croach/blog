6957393a2c0eb8f41da4439e07bacb685d53572d
Christopher Roach <vthakr@gmail.com>
2017-01-20 15:32:43 -0800

    Adding support for a custom domain
    My CNAME file was not being copied over, so I had to add it to
    STATIC_PATHS variable (to make sure that pelican copies it over to the
    output directory) and then add some metadata about how to copy it.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,9 @@ SITEURL = ''
 
 PATH = 'content'
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
+
 # The new github pages uses the master branch or the docs/ folder
 OUTPUT_PATH = 'docs/'
 
